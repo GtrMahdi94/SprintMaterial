b8c7a4f94a7a6a151272838c5a693f4ca075e5ff
Gatri Mahdi <gatrimahdi5@gmail.com>
2023-12-22 16:12:54 +0100

    news

--- FixTheError.js
+++ FixTheError.js
@@ -19,3 +19,4 @@ return x * x ;
         function square3 (x)  { 
             return x * x;
             }
+---------------------------------------
---
+++ Func_add.js
@@ -0,0 +1,3 @@
+function add(number1, number2) {
+    console.log (number1+number2);
+   }
